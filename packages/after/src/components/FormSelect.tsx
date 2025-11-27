7b38e30e4ce2b6def29e482a4c6cb389211cee83
Yongtae Park <samuel.park@mz.co.kr>
2025-11-28 03:30:23 +0900

    refactor: migrate FormSelect to native select element

--- packages/after/src/components/FormSelect.tsx
+++ packages/after/src/components/FormSelect.tsx
@@ -1,11 +1,6 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-  type selectTriggerVariants,
-} from './ui/select';
+import * as React from 'react';
+
+import { NativeSelect, NativeSelectOption, type nativeSelectVariants } from './ui/native-select';
 import { FormField, getFieldDescriptionId } from './ui/field';
 import type { VariantProps } from 'class-variance-authority';
 
@@ -14,66 +9,73 @@ interface Option {
   label: string;
 }
 
-interface FormSelectProps extends VariantProps<typeof selectTriggerVariants> {
+interface FormSelectProps
+  extends Omit<React.SelectHTMLAttributes<HTMLSelectElement>, 'size'>,
+    VariantProps<typeof nativeSelectVariants> {
   id: string;
-  value?: string;
-  onValueChange?: (value: string) => void;
   options: Option[];
   label?: string;
   placeholder?: string;
-  required?: boolean;
-  disabled?: boolean;
   invalid?: boolean;
   invalidText?: string;
   helpText?: string;
 }
 
-function FormSelect({
-  id,
-  value,
-  onValueChange,
-  options,
-  label,
-  placeholder = 'Select an option...',
-  required,
-  disabled,
-  invalid,
-  invalidText,
-  helpText,
-  size,
-}: FormSelectProps) {
-  const descriptionId = getFieldDescriptionId(id, !!(helpText || invalidText));
+const FormSelect = React.forwardRef<HTMLSelectElement, FormSelectProps>(
+  (
+    {
+      id,
+      options,
+      label,
+      placeholder = 'Select an option...',
+      required,
+      disabled,
+      invalid,
+      invalidText,
+      helpText,
+      size,
+      ...props
+    },
+    ref
+  ) => {
+    const descriptionId = getFieldDescriptionId(id, !!(helpText || invalidText));
 
-  return (
-    <FormField
-      id={id}
-      label={label}
-      helpText={helpText}
-      invalid={invalid}
-      invalidText={invalidText}
-      required={required}
-    >
-      <Select value={value} onValueChange={onValueChange} disabled={disabled} required={required}>
-        <SelectTrigger
+    return (
+      <FormField
+        id={id}
+        label={label}
+        helpText={helpText}
+        invalid={invalid}
+        invalidText={invalidText}
+        required={required}
+      >
+        <NativeSelect
           id={id}
+          ref={ref}
+          disabled={disabled}
+          required={required}
           variant={invalid ? 'error' : 'default'}
           size={size}
           aria-invalid={invalid || undefined}
           aria-describedby={descriptionId}
+          {...props}
         >
-          <SelectValue placeholder={placeholder} />
-        </SelectTrigger>
-        <SelectContent>
+          {placeholder && (
+            <NativeSelectOption value="" hidden>
+              {placeholder}
+            </NativeSelectOption>
+          )}
           {options.map(option => (
-            <SelectItem key={option.value} value={option.value}>
+            <NativeSelectOption key={option.value} value={option.value}>
               {option.label}
-            </SelectItem>
+            </NativeSelectOption>
           ))}
-        </SelectContent>
-      </Select>
-    </FormField>
-  );
-}
+        </NativeSelect>
+      </FormField>
+    );
+  }
+);
+FormSelect.displayName = 'FormSelect';
 
 export { FormSelect };
 export type { Option as SelectOption };
