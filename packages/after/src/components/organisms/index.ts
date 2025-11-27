b262ff32b0c40890dfc39ac00503f787e37028e6
천진아 <jncheon@carmerce.co.kr>
2025-11-28 01:48:49 +0900

    refactor: Alert 컴포넌트를 organisms에서 ui로 이동 및 variant 색상 적용
    - organisms/Alert.tsx를 ui/Alert.tsx로 통합
    - ui/Alert.tsx에 variant별 색상 적용 기능 추가 (children에 적용)
    - organisms/index.ts에서 Alert export를 ui/Alert로 변경
    - AlertDescription에 variant prop 추가하여 variant별 텍스트 색상 적용

--- packages/after/src/components/organisms/index.ts
+++ packages/after/src/components/organisms/index.ts
@@ -1,5 +1,6 @@
-export { Alert } from "../ui/Alert";
-export { Card } from "../ui/Card";
-export { Table } from "./Table";
-export { Modal } from "../ui/Modal";
-export { Header } from "../ui/Header";
+import { Alert } from '../ui/Alert';
+import { Card } from './Card';
+import { Modal } from './Modal';
+import { Header } from './Header';
+
+export { Alert, Card, Modal, Header };
