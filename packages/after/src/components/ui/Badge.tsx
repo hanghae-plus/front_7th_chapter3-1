ae9221a49a82b35d613c0260a0018bbf7b42f2a8
천진아 <jncheon@carmerce.co.kr>
2025-11-27 23:17:53 +0900

    style: 디자인 token 정리

--- packages/after/src/components/ui/Badge.tsx
+++ packages/after/src/components/ui/Badge.tsx
@@ -4,7 +4,6 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 
-// TODO: bg-success, bg-warning bg-info token 설정 필요
 const badgeVariants = cva(
 	'inline-flex items-center justify-center border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
 	{
@@ -15,16 +14,16 @@ const badgeVariants = cva(
 			},
 			type: {
 				primary:
-					'border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90',
+					'border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary-hover',
 				secondary:
-					'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
+					'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary-hover',
 				success:
-					'border-transparent bg-success text-success-foreground [a&]:hover:bg-success/90',
+					'border-transparent bg-success text-success-foreground [a&]:hover:bg-success-hover',
 				warning:
-					'border-transparent bg-warning text-warning-foreground [a&]:hover:bg-warning/90',
+					'border-transparent bg-warning text-warning-foreground [a&]:hover:bg-warning-hover',
 				danger:
-					'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-				info: 'border-transparent bg-info text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+					'border-transparent bg-error text-error-foreground [a&]:hover:bg-error-hover focus-visible:ring-error/20 dark:focus-visible:ring-error/40 dark:bg-error/60',
+				info: 'border-transparent bg-info text-info-foreground [a&]:hover:bg-info-hover focus-visible:ring-info/20 dark:focus-visible:ring-info/40 dark:bg-info/60',
 			},
 		},
 		defaultVariants: {
