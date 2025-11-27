ac373df161728f3c41cb33bd96006664d345c6c5
Woojung Kim <60292183+kimfriendship@users.noreply.github.com>
2025-11-27 13:44:58 +0900

    feat: 디자인 토큰 적용

--- packages/after/tailwind.config.ts
+++ packages/after/tailwind.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from "tailwindcss";
+import { tokens } from "./src/styles/tokens";
 
 const config: Config = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: tokens.colors,
+      fonts: tokens.fonts,
+      screens: tokens.breakpoints,
+    },
+  },
 };
 
 export default config;
-
