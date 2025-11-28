99e3db9aed314da0678c01ffda8eaa11dba051b6
천진아 <jncheon@carmerce.co.kr>
2025-11-28 22:33:10 +0900

    refactor: Header 컴포넌트 style 방식 tailwind로 통일

--- packages/after/src/App.tsx
+++ packages/after/src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Header } from './components/organisms';
-import { ManagementPage } from './pages/ManagementPage';
-import './styles/index.css';
-import { DialogProvider } from './components/composed/DialogProvider';
+import React from "react";
+import Header from "./components/composed/Header";
+import { ManagementPage } from "./pages/ManagementPage";
+import "./styles/index.css";
+import { DialogProvider } from "./components/composed/DialogProvider";
 
 export const App: React.FC = () => {
-	return (
-		<DialogProvider>
-			<div style={{ minHeight: '100vh', backgroundColor: '#f7fafc' }}>
-				<Header />
-				<main>
-					<ManagementPage />
-				</main>
-			</div>
-		</DialogProvider>
-	);
+  return (
+    <DialogProvider>
+      <div style={{ minHeight: "100vh", backgroundColor: "#f7fafc" }}>
+        <Header />
+        <main>
+          <ManagementPage />
+        </main>
+      </div>
+    </DialogProvider>
+  );
 };
--- packages/after/src/components/organisms/index.ts
+++
@@ -1,6 +0,0 @@
-import { Alert } from '../ui/alert';
-import { Card } from './Card';
-import { Modal } from '../ui/Modal';
-import { Header } from './Header';
-
-export { Alert, Card, Modal, Header };
