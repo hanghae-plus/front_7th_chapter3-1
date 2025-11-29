d065141eca8c96c6f365f3ea6cecf26ebc202b82
Yongtae Park <samuel.park@mz.co.kr>
2025-11-29 12:05:40 +0900

    build(deploy): update base URL for production environment

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-const base: string = process.env.NODE_ENV === 'production' ? '/front_7th_chapter3-1/' : '';
+const base: string = process.env.NODE_ENV === 'production' ? '/front_7th_chapter3-1/app/' : '';
 
 // https://vite.dev/config/
 export default defineConfig({
